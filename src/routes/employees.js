d52b2f1889fb949b3e02295b97d0548b583d21ce
didierrevelo <1829@holbertonschool.com>
2021-07-07 12:19:52 -0500

    trying create an api

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/', (req, res) => {
+router.get('/v1', (req, res) => {
     pool.query('SELECT * FROM links', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
